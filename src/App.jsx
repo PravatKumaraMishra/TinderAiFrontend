32df739c0dbc6841f3914d9ea5c54dec15209cfc
pravatkumarmishra <pravatkumarmishrapkm@gmail.com>
2026-08-11 21:24:59 +0530

    add match list ui

--- src/App.jsx
+++ src/App.jsx
@@ -30,13 +30,52 @@ export default function App() {
       </div>
     </div>
   );
+
+  const MatchesList = () => (
+    <div className="rounded-lg shadow-lg p-4">
+      <h2 className="text-2xl font-bold mb-4">Matches</h2>
+      <ul>
+        {[
+          {
+            id: 1,
+            firstName: "Shyamali",
+            lastName: "Das",
+            imageUrl:
+              "http://127.0.0.1:8081/3ab5dd01-35c3-41c3-8685-8f9c7ce44261.jpg",
+          },
+          {
+            id: 2,
+            firstName: "Piyali",
+            lastName: "Maity",
+            imageUrl:
+              "http://127.0.0.1:8081/b15ba4db-0430-4998-b9ab-7f3dca6b01ca.jpg",
+          },
+        ].map((match) => (
+          <li key={match.id} className="mb-2">
+            <button className="w-full hover:bg-gray-100 rounded flex item-center">
+              <img
+                src={match.imageUrl}
+                className="w-16 h-16 rounded-full mr-3 object-cover"
+              />
+              <span>
+                <h3 className="font-bold">
+                  {match.firstName} {match.lastName}
+                </h3>
+              </span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
   return (
     <div className="max-w-md mx-auto p-4">
       <nav className="flex justify-between mb-4">
         <User />
         <MessageCircle />
       </nav>
-      <ProfileSelector />
+      {/* <ProfileSelector /> */}
+      <MatchesList />
     </div>
   );
 }
